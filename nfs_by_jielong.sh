c6c8566f5a29446092874b28fa4bd7889926876d
jielong.lin <493164984@qq.com>
2017-07-05 18:49:28 +0800

    Commited by using jll.Git.sh @ 2017-07-05 18:49:28
    Changes as follows:
    A  nfs_by_jielong.sh
    M  readme.jll

---
+++ nfs_by_jielong.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright(c) 2016-2100.  root.  All rights reserved.
+#
+#   FileName:     nfs_by_jielong.sh
+#   Author:       root
+#   Email:        493164984@qq.com
+#   DateTime:     2017-07-05 18:45:28
+#   ModifiedTime: 2017-07-05 18:47:30
+
+ /bin/mount -v -t nfs -o nfsvers=3 xmnb4003161.tpvaoc.com:/works4tpv /home/jielong.lin/xmnb4003161.tpvaoc.com/works4tpv  2>/home/jielong.lin/error_nfs@$(/bin/date %Y_%m_%d__%H_%M_%S)
+
