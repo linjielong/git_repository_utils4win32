b7257835345d7d61db932df7f6a1bee782b50a27
jielong.lin <493164984@qq.com>
2017-07-05 18:30:50 +0800

    Commited by using jll.Git.sh @ 2017-07-05 18:30:50
    Changes as follows:
    M  readme.jll
    A  works4tpv_BY_WinNFSd.bat
    A  works4tpv_bat_Executor.vbe
    A  works4tpv_bat_Executor.vbe.lnk

---
+++ works4tpv_BY_WinNFSd.bat
@@ -0,0 +1,8 @@
+#Copyrithgs (c) 2017-2100.    jielong.lin@qq.com     All rights reserved.
+#
+#github.com\linjielong\git_repository_utils4win32\WinNFSd.exe . works4tpv
+
+cd D:\System\jielong.lin\Desktop\works4tpv\
+github.com\linjielong\git_repository_utils4win32\WinNFSd.exe . /works4tpv
+
+pause
